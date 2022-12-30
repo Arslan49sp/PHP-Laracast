43a8537cf299bc725287b32b514bde13e47419b5
Arslan Majeed <49227101+Arslan49sp@users.noreply.github.com>
2022-12-31 01:31:54 +0500

    Styling completed ep:14

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,27 +1,4 @@
 <?php
-$books = [
-    [
-        'name' => 'Do Androids Dream of Electric Sheep',
-        'author' => 'Philip K. Dick',
-        'releaseYear' => 1968,
-        'purchaseUrl' => 'http://example.com'
-    ],
-    [
-        'name' => 'Project Hail Mary',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2021,
-        'purchaseUrl' => 'http://example.com'
-    ],
-    [
-        'name' => 'The Martian',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2011,
-        'purchaseUrl' => 'http://example.com'
-    ],
-];
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['author'] === 'Andy Weir';
-});
-
-require "index.view.php";
+require 'functions.php';
+$heading = 'Home';
+require "views/index.view.php";
