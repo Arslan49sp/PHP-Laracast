7102f713c08f47cff2666c34f0accdd1e7361a16
Arslan Majeed <49227101+Arslan49sp@users.noreply.github.com>
2023-01-02 23:58:47 +0500

    Section 2 completed

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,4 +1,18 @@
 <?php
 require 'functions.php';
-$heading = 'Home';
-require "views/index.view.php";
+require 'Database.php';
+
+$config = require('config.php');
+$db = new Database($config['database']);
+$id = $_GET['id'];
+if ($id != null) {
+    $query = "select * from posts where id = :id";
+    $posts = $db->query($query, [':id' => $id])->fetchAll();
+} else {
+    $query = "select * from posts";
+    $posts = $db->query($query)->fetchAll();
+}
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
